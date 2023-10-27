ba90217b2cb37c7c90f4c957e05dbcd224f6e015
AlpiGun5 <a.i.guncan02@gmail.com>
2023-10-27 14:54:45 +0200

    Refactor IDE formatting

--- Lab 1/Sokoban4j/src/MyAgent.java
+++ Lab 1/Sokoban4j/src/MyAgent.java
@@ -32,9 +32,9 @@ public class MyAgent extends ArtificialAgent {
 
     protected BoardSlim board;
     protected int searchedNodes;
-    protected List<Point> goals;
-    protected static int[] dirs = new int[]{-1,0,1,0};
-    protected DeadSquareDetector dsd;
+    private List<Point> goals;
+    private DeadSquareDetector dsd;
+
     @Override
     protected List<EDirection> think(BoardCompact board) {
         this.board = board.makeBoardSlim();
@@ -43,13 +43,13 @@ public class MyAgent extends ArtificialAgent {
         this.dsd = new DeadSquareDetector(this.board);
 
         // Start from 1 (or higher) for less risky solver
-        int corralRisk = 0;
+        int corralRisk = 0, maxCost = 500;
         long searchStartMillis = System.currentTimeMillis();
         while (corralRisk <= this.board.boxCount) {
             // Clear cache from old corrals
             dsd.corralCache = dsd.corralCache.entrySet().stream().filter(e -> !e.getValue())
                     .collect(Collectors.toMap(Map.Entry::getKey, stringBooleanEntry -> false));
-            List<EDirection> result = a_star(500, corralRisk++); // depth of search tree
+            List<EDirection> result = a_star(maxCost, corralRisk++); // depth of search tree
             if (result == null) continue;
             long searchTime = System.currentTimeMillis() - searchStartMillis;
             if (verbose) {
@@ -67,6 +67,7 @@ public class MyAgent extends ArtificialAgent {
         Map<Node, Integer> dist = new HashMap<>();
         Queue<Node> q = new PriorityQueue<>();
         BoxPoint[] boxes = findBoxes(board);
+        dsd.skipped = new int[]{0, 0, 0};
         dsd.corralRisk = corralRisk;
         boolean completed = false;
         Node start = new Node(boxes, board, null, null, 0, greedyMatching(boxes, goals));
@@ -78,7 +79,8 @@ public class MyAgent extends ArtificialAgent {
             curr = q.poll();
             searchedNodes++;
             // Guard clauses
-            if (completed = curr.board.isVictory()) break;
+            completed = curr.board.isVictory();
+            if (completed) break;
             if (curr.g > maxCost) continue;
             List<SAction> actions = new ArrayList<>(4);
             // Add possible moves
@@ -105,7 +107,10 @@ public class MyAgent extends ArtificialAgent {
                     continue;
                 dist.put(next, newCost);
                 // Update next state
-                next.parent = curr; next.pa = action; next.g = newCost; next.h = h(next.boxes, goals, mBox, curr.h);
+                next.parent = curr;
+                next.pa = action;
+                next.g = newCost;
+                next.h = h(next.boxes, goals, mBox, curr.h);
                 q.add(next);
             }
         }
@@ -120,6 +125,7 @@ public class MyAgent extends ArtificialAgent {
         System.out.println(actions.stream().map(o -> o.toString().substring(0, 1)).collect(Collectors.joining()));
         return actions;
     }
+
     // Heuristic function
     private double h(BoxPoint[] boxes, List<Point> goals, BoxPoint changed, double oldH) {
         // If boxes did not change, do not update
@@ -144,6 +150,7 @@ public class MyAgent extends ArtificialAgent {
         changed.dist = newDist;
         return res + changed.dist;
     }
+
     // State
     static class Node implements Comparable<Node>, Cloneable {
         BoxPoint[] boxes;
@@ -152,6 +159,7 @@ public class MyAgent extends ArtificialAgent {
         SAction pa;
         int g, hash;
         double h;
+
         public Node(BoxPoint[] boxes, BoardSlim board, Node parent, SAction pa, int g, double h) {
             this.boxes = boxes;
             this.board = board;
@@ -174,12 +182,13 @@ public class MyAgent extends ArtificialAgent {
         public BoxPoint moveBox(int x, int y, int tx, int ty) {
             board.moveBox((byte) x, (byte) y, (byte) tx, (byte) ty);
             BoxPoint box = null;
-            for (BoxPoint b : boxes) if (b.x == x && b.y == y) {
-                box = b;
-                box.x = tx;
-                box.y = ty;
-                break;
-            }
+            for (BoxPoint b : boxes)
+                if (b.x == x && b.y == y) {
+                    box = b;
+                    box.x = tx;
+                    box.y = ty;
+                    break;
+                }
             Arrays.sort(boxes, (l, r) -> l.x == r.x ? r.y - l.y : r.x - l.x);
             hash = -1;
             return box;
@@ -201,35 +210,42 @@ public class MyAgent extends ArtificialAgent {
             return Double.compare(this.g + this.h, o.g + o.h);
         }
     }
+
     // Box point extension with two extra variables (id of closest goal, and distance to it)
     static class BoxPoint extends Point {
         double dist;
         int closestGoalId;
+
         public BoxPoint(int x, int y, double dist, int closestGoalId) {
             super(x, y);
             this.dist = dist;
             this.closestGoalId = closestGoalId;
         }
     }
+
     // Point record
     static class Point {
         int x, y;
+
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
+
     // Class for finding dead squares
     static class DeadSquareDetector {
         boolean[][] dead;
-        int[] dirs = new int[]{-1,0,1,0}, skipped = new int[]{0, 0, 0};
+        int[] dirs = new int[]{-1, 0, 1, 0}, skipped = new int[]{0, 0, 0};
         byte obst = STile.WALL_FLAG | STile.BOX_FLAG;
         boolean corral = true;
         int corralRisk = 0, corralBoxes = 0, corralGoals = 0;
         Map<String, Boolean> freezeCache = new HashMap<>(), corralCache = new HashMap<>();
+
         public DeadSquareDetector(BoardSlim board) {
             this.dead = DeadSquareDetector.detectSimple(board);
         }
+
         // Detect simple deadlocks (static) - tiles from which a box cannot move, independent of other boxes
         public static boolean[][] detectSimple(BoardSlim board) {
             boolean[][] res = new boolean[board.width()][board.height()];
@@ -239,11 +255,13 @@ public class MyAgent extends ArtificialAgent {
             for (int i = 0; i < board.width(); i++) for (int j = 0; j < board.height(); j++) res[i][j] ^= true;
             return res;
         }
+
         private boolean detectSimple(int x, int y) {
             boolean res = dead[x][y];
             if (res) this.skipped[0]++;
             return res;
         }
+
         private static void pull(BoardSlim board, boolean[][] res, int x, int y, int[] dirs) {
             res[x][y] = true;
             for (int i = 0; i < 4; i++) {
@@ -251,11 +269,12 @@ public class MyAgent extends ArtificialAgent {
                 // Check bounds
                 if (nx < 1 || ny < 1 || nx > res.length - 2 || ny > res[0].length - 2) continue;
                 // Check that it can be pulled (two non-walls in the direction)
-                if (res[nx][ny] || STile.isWall(board.tiles[nx][ny])||
+                if (res[nx][ny] || STile.isWall(board.tiles[nx][ny]) ||
                         STile.isWall(board.tiles[nx + dirs[i]][ny + dirs[(i + 1) % 4]])) continue;
                 pull(board, res, nx, ny, dirs);
             }
         }
+
         // Detect freeze deadlocks (dynamic) - tiles from which a box cannot move, depends on other boxes
         public boolean detectFreeze(BoardSlim board, int x, int y, BoxPoint[] boxes) {
             // Return cached config if possible
@@ -270,6 +289,7 @@ public class MyAgent extends ArtificialAgent {
             freezeCache.put(k, res);
             return res;
         }
+
         private boolean detectFreeze(BoardSlim board, int x, int y, Set<Point> f) {
             // Check if frozen in x- and y-axis
             boolean[] frozen = new boolean[2];
@@ -279,14 +299,15 @@ public class MyAgent extends ArtificialAgent {
                 frozen[i] = STile.isWall(board.tiles[dx][dy]) || STile.isWall(board.tiles[ddx][ddy]) ||
                         (dead[dx][dy] && dead[ddx][ddy]);
             }
-            for (int i = 0; i < 2; i++) if (frozen[i]) {
-                // Prevent circular check
-                board.tiles[x][y] = STile.WALL_FLAG;
-                int dy = y + dirs[i], dx = x + dirs[i + 1], ddy = y + dirs[i + 2], ddx = x + dirs[(i + 3) % 4];
-                // If box -> recursively check if next box is frozen
-                if (STile.isBox(board.tiles[dx][dy])) frozen[1 - i] = detectFreeze(board, dx, dy, f);
-                if (STile.isBox(board.tiles[ddx][ddy])) frozen[1 - i] = detectFreeze(board, ddx, ddy, f);
-            }
+            for (int i = 0; i < 2; i++)
+                if (frozen[i]) {
+                    // Prevent circular check
+                    board.tiles[x][y] = STile.WALL_FLAG;
+                    int dy = y + dirs[i], dx = x + dirs[i + 1], ddy = y + dirs[i + 2], ddx = x + dirs[(i + 3) % 4];
+                    // If box -> recursively check if next box is frozen
+                    if (STile.isBox(board.tiles[dx][dy])) frozen[1 - i] = detectFreeze(board, dx, dy, f);
+                    if (STile.isBox(board.tiles[ddx][ddy])) frozen[1 - i] = detectFreeze(board, ddx, ddy, f);
+                }
             // If frozen from both axes
             if (frozen[0] && frozen[1]) {
                 f.add(new Point(x, y));
@@ -294,6 +315,7 @@ public class MyAgent extends ArtificialAgent {
             }
             return false;
         }
+
         // Detect corral deadlocks - when pushed box forms a closed unreachable area with not enough goals
         public boolean detectCorral(BoardSlim board, int x, int y, int dx, int dy, BoxPoint[] boxes) {
             if (corralRisk == board.boxCount) return false;
@@ -303,7 +325,8 @@ public class MyAgent extends ArtificialAgent {
             // If neighboring tiles are not obstacles, cannot form a coral
             if ((board.tiles[x - dy][y - dx] & obst) == 0 || (board.tiles[x + dy][y + dx] & obst) == 0) return false;
             if ((board.tiles[x + dx][y + dy] & obst) != 0) return false;
-            corral = true; corralBoxes = 0; corralGoals = 0;
+            corral = true;
+            corralBoxes = corralGoals = 0;
             floodFill(board, x + dx, y + dy, new boolean[board.width()][board.height()]);
             // If player or enough goals inside coral, then may be solvable
             boolean res = corral && corralGoals + corralRisk < corralBoxes;
@@ -311,6 +334,7 @@ public class MyAgent extends ArtificialAgent {
             corralCache.put(k, res);
             return res;
         }
+
         private void floodFill(BoardSlim board, int x, int y, boolean[][] seen) {
             seen[x][y] = true;
             // Check for player
@@ -335,15 +359,19 @@ public class MyAgent extends ArtificialAgent {
             }
         }
     }
+
     // Helper that matches each box to its closest goal, taking other matches into account
     // Returns heuristic (sum of Manhattan distances of matches)
-    private static int greedyMatching(BoxPoint[] boxes, List<Point> goals) {
+    private static double greedyMatching(BoxPoint[] boxes, List<Point> goals) {
         Queue<BoxPoint> pq = new PriorityQueue<>(Comparator.comparing(a -> a.dist));
-        for (int i = 0; i < boxes.length; i++) for (int j = 0; j < goals.size(); j++) {
-            BoxPoint b = boxes[i]; Point g = goals.get(j);
-            pq.add(new BoxPoint(i, j, Math.abs(b.x - g.x) + Math.abs(b.y - g.y), -1));
-        }
-        int matchedBoxes = 0, matchedGoals = 0, res = 0;
+        for (int i = 0; i < boxes.length; i++)
+            for (int j = 0; j < goals.size(); j++) {
+                BoxPoint b = boxes[i];
+                Point g = goals.get(j);
+                pq.add(new BoxPoint(i, j, Math.abs(b.x - g.x) + Math.abs(b.y - g.y), -1));
+            }
+        int matchedBoxes = 0, matchedGoals = 0;
+        double res = 0.0;
         while (!pq.isEmpty()) {
             BoxPoint curr = pq.poll();
             if (((matchedBoxes & (1 << curr.x)) != 0) || ((matchedGoals & (1 << curr.y)) != 0)) continue;
@@ -355,18 +383,22 @@ public class MyAgent extends ArtificialAgent {
         }
         return res;
     }
+
     // Helper for finding all goals in a board
     private static List<Point> findGoals(BoardSlim board) {
         List<Point> res = new ArrayList<>();
-        for (int i = 1; i < board.width() - 1; i++) for (int j = 1; j < board.height() - 1; j++)
-            if ((STile.PLACE_FLAG & board.tiles[i][j]) != 0) res.add(new Point(i, j));
+        for (int i = 1; i < board.width() - 1; i++)
+            for (int j = 1; j < board.height() - 1; j++)
+                if ((STile.PLACE_FLAG & board.tiles[i][j]) != 0) res.add(new Point(i, j));
         return res;
     }
+
     // Helper for finding all boxes in a board
     private static BoxPoint[] findBoxes(BoardSlim board) {
         List<BoxPoint> res = new ArrayList<>();
-        for (int i = 1; i < board.width() - 1; i++) for (int j = 1; j < board.height() - 1; j++)
-            if ((STile.BOX_FLAG & board.tiles[i][j]) != 0) res.add(new BoxPoint(i, j, -1, -1));
+        for (int i = 1; i < board.width() - 1; i++)
+            for (int j = 1; j < board.height() - 1; j++)
+                if ((STile.BOX_FLAG & board.tiles[i][j]) != 0) res.add(new BoxPoint(i, j, -1, -1));
         res.sort((l, r) -> l.x == r.x ? r.y - l.y : r.x - l.x);
         return res.toArray(BoxPoint[]::new);
     }
