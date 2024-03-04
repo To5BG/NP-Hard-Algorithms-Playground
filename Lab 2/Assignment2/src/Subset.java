cf589793177e264ec116c17740b6cccd911dad66
AlpiGun5 <a.i.guncan02@gmail.com>
2024-03-04 01:52:08 +0100

    gg ez

--- Lab 2/Assignment2/src/Subset.java
+++ Lab 2/Assignment2/src/Subset.java
@@ -3,6 +3,7 @@ import solver.Problem;
 import solver.Solver;
 import solver.VariableBind;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -17,19 +18,20 @@ public class Subset {
 
         CSolution<int[]> res = new Solver<int[]>()
                 .addParameter("N", null)
-                .addVariable("comb", new VariableBind(
-                        List.of("N"), i -> IntStream.rangeClosed(0, (Integer) i.get(0))
-                        .boxed().collect(Collectors.toList()),
+                .addVariable("comb", new VariableBind(List.of(), i -> new ArrayList<>(List.of(0, 1)),
                         List.of("N"), i -> {
                     Integer[] v = new Integer[(Integer) i.get(0)];
                     Arrays.fill(v, Integer.MIN_VALUE);
                     return v;
                 }))
-                .addConstraint("comb", "alldiff", 0)
-                .addConstraint("comb", "decrease", -1)
-                .setVariableSelection(true, false)
-                .solve(model, Problem.ALL, m -> Arrays.stream(m.get("comb")).filter(i -> i != 0)
-                        .mapToInt(Integer::intValue).toArray(), null);
+                .solve(model, Problem.ALL, m -> {
+                    List<Integer> r = new ArrayList<>();
+                    Integer[] mr = m.get("comb");
+                    for (int i = 0; i < mr.length; i++) if (mr[i] != 0) r.add(i + 1);
+                    int[] rr = new int[r.size()];
+                    for (int i = 0; i < r.size(); i++) rr[i] = r.get(i);
+                    return rr;
+                }, null);
 
         System.out.println("=====SOLUTION=====");
         System.out.println(res.count);
